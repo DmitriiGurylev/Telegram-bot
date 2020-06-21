bf0a6e9baf4f63779c5eec51b3e36991f1ca54a4
Wonderful-guy <67173663+Wonderful-guy@users.noreply.github.com>
2020-06-22 02:57:10 +0300

    first commit

---
+++ config.py
@@ -0,0 +1,4 @@
+##  здесь хранится токен бота и API банка, с которого взяты валюты разных стран
+
+token = '1104755099:AAE85nbvyYOkKZIOJo30VvCM3ophDbSwALA'
+url_currency = 'https://www.cbr-xml-daily.ru/daily_json.js'
