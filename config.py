ff5847cc48177a254fd0f6bc45a11d29dac75349
Wonderful-guy <67173663+Wonderful-guy@users.noreply.github.com>
2020-06-22 07:41:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-##  здесь хранится токен бота и API банка, с которого взяты валюты разных стран
+#  There is bot token and bank API
+#  здесь хранится токен бота и API банка, с которого взяты валюты разных стран
 
 token = '1104755099:AAE85nbvyYOkKZIOJo30VvCM3ophDbSwALA'
 url_currency = 'https://www.cbr-xml-daily.ru/daily_json.js'
