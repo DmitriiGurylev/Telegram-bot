44a8b25d95fc9a6cc2f11c82b41bf56ffa25377c
d.gurylev <d.gurylev@tdm-tech.ru>
2023-02-01 12:47:35 +0300

    addd .gitignore

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ twitter_access_key = '1464562711019274240-Lt0W9MrM66xUyUGQM9geiaUhrvlAdp'
 twitter_access_secret = 'wJ0ZxewOfYYIxZtZFcY64yJllNWrRAHM5PvqoqfdjpY6q'
 twitter_bearer_token = 'AAAAAAAAAAAAAAAAAAAAAI%2F3WAEAAAAAf%2FKXeYv1gCmPxs09gYiMp5b5j0Y%3D61JqNSGDTkl6sEkGMUCIKrYV82SX8HCqqtgZHvU5hbo2g4V2AR'
 
-# bank
-bank_api_currency = 'https://www.cbr-xml-daily.ru/daily_json.js'
+# # bank
+# bank_api_currency = 'https://www.cbr-xml-daily.ru/daily_json.js'
 
 # chat id
 chat_id ='416544613'
