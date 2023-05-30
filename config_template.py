702b0b425e1327059c4e23288b1a3a887989a162
d.gurylev <d.gurylev@tdm-tech.ru>
2023-05-30 17:13:31 +0800

    added config_template.py

---
+++ config_template.py
@@ -0,0 +1,13 @@
+#  There is bot token, Twotter keys
+#  здесь хранится токен бота, ключи Twitter
+import logging
+
+# Telegram-bot token
+telegram_bot_token = 'token'
+
+# twitter
+twitter_consumer_key = 'key'
+twitter_consumer_secret = 'secret'
+twitter_access_key = 'key'
+twitter_access_secret = 'secret'
+twitter_bearer_token = 'token'
